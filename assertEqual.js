2deea66b011a6df1f5ea987138564d98f07f8918
Zackory Kelly <zackkelly@live.ca>
2021-03-29 18:56:06 +0000

    Updated assertEqual to use template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 const mad = String.fromCodePoint(0x1f621);
 const smile = String.fromCodePoint(0x1f600);
 
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log(smile + smile + smile + 'Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`${smile}${smile}${smile} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(mad + mad + mad + 'Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`${mad}${mad}${mad} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
