45c74baa79ec77ed70f8bfc84d45309f9fa4efaa
Zackory Kelly <zackkelly@live.ca>
2021-03-29 18:30:42 +0000

    Completed assertEqual to check 2 inputs for equality

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const mad = String.fromCodePoint(0x1f621);
+const smile = String.fromCodePoint(0x1f600);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(smile + smile + smile + 'Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log(mad + mad + mad + 'Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+//Test
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(1.1, 1);
