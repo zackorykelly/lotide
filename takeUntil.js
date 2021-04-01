fb1cb46f2ca4cfb8d01db2b9b90f558074147847
Zackory Kelly <zackkelly@live.ca>
2021-04-01 23:00:09 +0000

    Implemented takeUntil function for lotide

--- takeUntil.js
+++ takeUntil.js
@@ -21,20 +21,24 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-const map = function(array, callback) {
-  const results = [];
-
-  for (let item of array) {
-    results.push(callback(item));
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (const value of array) {
+    if (callback(value)) {
+      return result;
+    } else {
+      result.push(value);
+    }
   }
-  return results;
+  return result;
 };
 
-const words = ["ground", "control", "to", "major", "tom"];
-const results1 = map(words, word => word[0]);
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
-const results2 = map([1, 2, 3], item => item + 1);
-console.log(results2);
 
-assertArraysEqual(map([1, 2, 3], item => item + 1), [2, 3, 4]);
-assertArraysEqual(map(['apple', 'bear', 'bat'], item => item[1]), ['p', 'e', 'a']);
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
