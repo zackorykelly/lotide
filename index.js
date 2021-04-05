551b28d77e4db00f318cf9d2a6f8b8dcf34d4ec8
Zackory Kelly <zackkelly@live.ca>
2021-04-05 18:16:25 +0000

    Modified index using object shorthand

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ const without = require('./without');
 
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPosition: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
